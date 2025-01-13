2e28285e3401b8b86eeee53a9fea9c1b7cd870f2
Radheshyam1234 <rdk7352669258@gmail.com>
2025-01-13 22:10:08 +0530

    feat: added address update ui

--- src/views/checkout/components/checkout-items-container/checkout-items-container.tsx
+++ src/views/checkout/components/checkout-items-container/checkout-items-container.tsx
@@ -1,19 +1,33 @@
 import { DefaultProductImageUrl } from 'constants/index.const'
+import ChevronDownIcon from "assets/icons/chevron-down-gray.svg"
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Button from 'common-components/button/button'
 
 type Props = {}
 
 const CheckoutItemsContainer = (props: Props) => {
+  const [showItems, setShowItems] = useState(false)
+
+  useEffect(() => {
+    if (window.innerWidth <= 1024) setShowItems(false)
+    else setShowItems(true)
+  }, [])
+
   return (
-    <div className='space-y-5'>
-      <p className="text-lg text-gray-900 font-bold">4 Item</p>
-      <div className="space-y-4">
+    <div className=' pb-4 border-b lg:border-none border-gray-300 '>
+      <Button className="px-3 py-1.5 lg:hidden w-full  flex justify-between items-center hover:bg-gray-50 rounded-md" onClick={() => { setShowItems(!showItems) }}>
+        <p className="text-lg text-gray-900 font-bold">4 Item</p>
+        <Image src={ChevronDownIcon} alt="arrow-down.svg" />
+      </Button>
+
+      <p className="hidden lg:block text-lg text-gray-900 font-bold">4 Item</p>
+      <div className={`space-y-4 ${showItems ? "max-h-[1000px]" : "max-h-0"} overflow-hidden transition-all duration-300`}>
         {
           [1, 2, 3,].map((_, index) => {
             return (
               <div className="mt-3 p-3 rounded-md border border-gray-200 flex items-center gap-4" key={index}>
-                <Image src={DefaultProductImageUrl} alt="prduct.png" height={120} width={100} className="max-w-28 h-auto rounded-md" />
+                <Image src={DefaultProductImageUrl} alt="prduct.png" height={120} width={100} className="max-w-20 lg:max-w-28 h-auto rounded-md" />
                 <div className="space-y-1">
                   <p className="text-sm font-bold text-gray-900">Lavender evil eye set Straight Winter friendly suits sets</p>
                   <p className="text-sm font-semibold text-gray-600">Sixe: L/Qty:3</p>
